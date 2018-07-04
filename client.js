70be9a8371b03b15dc3929fe2a42f1e746bb8c7a
alamsha <alamsha@users.noreply.github.com>
2018-07-04 21:09:24 +0400

    Update client.js

--- client.js
+++ client.js
@@ -1,17 +1,29 @@
 var net = require('net');
 
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
 var client = new net.Socket();
-client.connect(1337, 'localhost', function() {
-	console.log('Connected');
-	client.write('Hello, TCP passive server at localhost:1337! Love, Client.');
+client.connect(PORT, HOST, function() {
+
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    client.write('Hello, TCP passive server at localhost:1337! Love, Client.');
 
 });
 
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
 client.on('data', function(data) {
-   console.log(data.toString());
-   client.end();
+    
+    console.log('DATA: ' + data);
+    // Close the client socket completely
+    client.destroy();
+    
 });
-client.on('end', function() { 
-   console.log('disconnected from server');
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
 });
 
