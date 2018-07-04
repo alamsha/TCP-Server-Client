267336d9aebc8b74daad8da3b21f49a919be0322
alamsha <alamsha@users.noreply.github.com>
2018-07-04 18:41:10 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-/*
-In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
-server, but for some reason omit a client connecting to it.  I added an 
-example at the bottom.
-
-Save the following server in example.js:
-*/
-
 var net = require('net');
-
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
-	socket.pipe(socket);
+var server = net.createServer(function(connection) { 
+   console.log('client connected');
+   
+   connection.on('end', function() {
+      console.log('client disconnected');
+   });
+   connection.write('Hello World!\r\n');
+   connection.pipe(connection);
 });
-
-server.listen(1337, 'localhost');
-
+server.listen(1337, 'localhost', function() { 
+   console.log('server is listening');
+});
